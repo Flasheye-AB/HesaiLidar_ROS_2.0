e07bf1737bbe0ce591cfa2d665412025774cec37
Johan Forsberg <johan.forsberg@flasheye.se>
2026-01-09 00:53:50 +0100

    Remake config sets first or last from dual return scan parameter in config.yaml

--- src/manager/source_drive_common.hpp
+++ src/manager/source_drive_common.hpp
@@ -80,7 +80,8 @@ public:
                            driver_param.decoder_param.remake_config.use_ring_for_vertical, false);
             YamlRead<bool>(driver_config["remake_config"], "duplicate_sparse_rings",
                            driver_param.decoder_param.remake_config.duplicate_sparse_rings, false);
-        }
+            YamlRead<int>(driver_config["remake_config"], "echo_mode_filter",
+                          driver_param.decoder_param.echo_mode_filter, 0);        }
         // ROS related
         YamlRead<bool>(       config["ros"], "send_packet_ros",            driver_param.input_param.send_packet_ros, false);
         YamlRead<bool>(       config["ros"], "send_point_cloud_ros",       driver_param.input_param.send_point_cloud_ros, false);
